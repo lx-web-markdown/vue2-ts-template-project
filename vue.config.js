fc5433209235550d111b6b76d0e87a1a94abdbc1
linxiang <lionsom.linx@qq.com>
2025-07-21 10:25:24 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue2-demos/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   /*
   // 启用模板编译器（会增加包体积）
   runtimeCompiler: true, 
