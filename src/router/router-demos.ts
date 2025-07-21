a5f2d203346d7eaf6373788672efa01293a3f6c2
linxiang <lionsom.linx@qq.com>
2025-07-21 10:09:23 +0800

    init

---
+++ src/router/router-demos.ts
@@ -0,0 +1,6 @@
+export default [
+  {
+    path: '/i18n-demo',
+    component: () => import('@/views/modules/i18n-page/index.vue')
+  }
+]
---
+++ src/views/modules/auth/login-page.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>登录</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LoginPage',
+  data() {
+    return {}
+  },
+  mounted() {
+    console.log('LoginPage mounted')
+  },
+  methods: {
+    login() {
+      this.$router.push('/')
+    }
+  }
+})
+</script>
+
+<style scoped></style>
